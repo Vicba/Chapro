1e17b521d7ca11e998f53147becb96e49e8f1933
victor.barra <victor.barra@student.howest.be>
2023-06-27 21:33:31 +0200

    dashboard, project page responsive

--- src/Components/Sidebar.jsx
+++ src/Components/Sidebar.jsx
@@ -1,60 +1,60 @@
 //components
-import Avatar from './Avatar'
+import Avatar from "./Avatar";
 
-import { useAuthContext } from '../hooks/useAuthContext'
+import { useAuthContext } from "../hooks/useAuthContext";
 
 //styles & images
-import './Sidebar.css'
-import DashboardIcon from '../assets/dashboard_icon.svg'
-import AddIcon from '../assets/add_icon.svg'
-import Friends from '../assets/friends.png'
-import Settings from '../assets/settings.svg'
-import NameLogo from '../assets/name-logo.png'
+import "./Sidebar.css";
+import DashboardIcon from "../assets/dashboard_icon.svg";
+import AddIcon from "../assets/add_icon.svg";
+import Friends from "../assets/friends.png";
+import Settings from "../assets/settings.svg";
+import NameLogo from "../assets/name-logo.png";
 
-import React, { useState } from 'react'
-import { NavLink } from 'react-router-dom'
-import { useDocument } from '../hooks/useDocument'
+import React, { useState } from "react";
+import { NavLink } from "react-router-dom";
+import { useDocument } from "../hooks/useDocument";
 
 export default function Sidebar() {
-    const { user } = useAuthContext()
+  const { user } = useAuthContext();
 
-    return (
-        <div className='sidebar'>
-            <div className="sidebar-content">
-                <div className="user">
-                    <img src={NameLogo} alt='name-logo' className='logo' />
-                    <Avatar src={user.photoURL} />
-                </div>
-
-                <nav className='links'>
-                    <ul>
-                        <li>
-                            <NavLink to='/'>
-                                <img src={DashboardIcon} alt="dashboard icon" />
-                                <span>Dashboard</span>
-                            </NavLink>
-                        </li>
-                        <li>
-                            <NavLink to='/create'>
-                                <img src={AddIcon} alt="add project icon" />
-                                <span>New project</span>
-                            </NavLink>
-                        </li>
-                        <li>
-                            <NavLink to='/friends'>
-                                <img src={Friends} alt="Friends" />
-                                <span>Friends</span>
-                            </NavLink>
-                        </li>
-                        <li>
-                            <NavLink to='/settings'>
-                                <img src={Settings} alt="settings" className='settings-img' />
-                                <span>Settings</span>
-                            </NavLink>
-                        </li>
-                    </ul>
-                </nav>
-            </div>
+  return (
+    <div className="sidebar">
+      <div className="sidebar-content">
+        <div className="user">
+          <img src={NameLogo} alt="name-logo" className="logo" />
+          <Avatar src={user.photoURL} />
         </div>
-    )
+
+        <nav className="links">
+          <ul>
+            <li>
+              <NavLink to="/">
+                <img src={DashboardIcon} alt="dashboard icon" />
+                <span>Dashboard</span>
+              </NavLink>
+            </li>
+            <li>
+              <NavLink to="/create">
+                <img src={AddIcon} alt="add project icon" />
+                <span>New project</span>
+              </NavLink>
+            </li>
+            <li>
+              <NavLink to="/friends">
+                <img src={Friends} alt="Friends" />
+                <span>Friends</span>
+              </NavLink>
+            </li>
+            <li>
+              <NavLink to="/settings">
+                <img src={Settings} alt="settings" className="settings-img" />
+                <span>Settings</span>
+              </NavLink>
+            </li>
+          </ul>
+        </nav>
+      </div>
+    </div>
+  );
 }
--- src/pages/dashboard/Dashboard.jsx
+++ src/pages/dashboard/Dashboard.jsx
@@ -1,63 +1,70 @@
-import { useCollection } from '../../hooks/useCollection'
-import { useState } from 'react'
-import { useAuthContext } from '../../hooks/useAuthContext'
+import { useCollection } from "../../hooks/useCollection";
+import { useState } from "react";
+import { useAuthContext } from "../../hooks/useAuthContext";
 
 // components
-import ProjectList from '../../Components/ProjectList'
-import ProjectFilter from './ProjectFilter'
+import ProjectList from "../../Components/ProjectList";
+import ProjectFilter from "./ProjectFilter";
 
 // styles
-import './Dashboard.css'
-import { uzCyrl } from 'date-fns/locale'
+import "./Dashboard.css";
+import { uzCyrl } from "date-fns/locale";
 
 export default function Dashboard() {
-    const { user } = useAuthContext()
+  const { user } = useAuthContext();
 
-    const { documents, error } = useCollection('projects')
+  const { documents, error } = useCollection("projects");
 
-    const [currentFilter, setCurrentFilter] = useState('All')
+  const [currentFilter, setCurrentFilter] = useState("All");
 
-    const changeFilter = (newFilter) => {
-        setCurrentFilter(newFilter)
-    }
+  const changeFilter = (newFilter) => {
+    setCurrentFilter(newFilter);
+  };
 
-    const projects = documents ? documents.filter(document => {
+  const projects = documents
+    ? documents.filter((document) => {
         switch (currentFilter) {
-            case 'All':
-                let assignedToMe = false
-                if (document.createdBy.id === user.uid) {
-                    assignedToMe = true
-                }
-                document.assignedUsersList.forEach(u => {
-                    if (u.id === user.uid) {
-                        assignedToMe = true
-                    }
-                })
-                return assignedToMe
-            case 'UI/UX':
-            case 'Frontend':
-            case 'Backend':
-            case 'Databases':
-            case 'DevOps':
-            case "ML/AI":
-                let forMe = false
-                document.assignedUsersList.forEach(u => {
-                    if (u.id === user.uid) {
-                        forMe = true
-                    }
-                })
-                return forMe && document.category === currentFilter
-            default:
-                return true
+          case "All":
+            let assignedToMe = false;
+            if (document.createdBy.id === user.uid) {
+              assignedToMe = true;
+            }
+            document.assignedUsersList.forEach((u) => {
+              if (u.id === user.uid) {
+                assignedToMe = true;
+              }
+            });
+            return assignedToMe;
+          case "UI/UX":
+          case "Frontend":
+          case "Backend":
+          case "Databases":
+          case "DevOps":
+          case "ML/AI":
+            let forMe = false;
+            document.assignedUsersList.forEach((u) => {
+              if (u.id === user.uid) {
+                forMe = true;
+              }
+            });
+            return forMe && document.category === currentFilter;
+          default:
+            return true;
         }
-    }) : null
+      })
+    : null;
 
-    return (
-        <div>
-            <h2 className="page-title">Dashboard</h2>
-            {error && <p className="error">{error}</p>}
-            {documents && <ProjectFilter currentFilter={currentFilter} changeFilter={changeFilter} />}
-            {projects && <ProjectList projects={projects} />}
-        </div>
-    )
+  return (
+    <div className="container">
+      <h2 className="page-title">Dashboard</h2>
+      {error && <p className="error">{error}</p>}
+      {documents && (
+        <ProjectFilter
+          currentFilter={currentFilter}
+          changeFilter={changeFilter}
+        />
+      )}
+      {projects && <ProjectList projects={projects} />}
+    </div>
+  );
 }
